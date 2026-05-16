ae3b6591e0dcf8bac17f1485ecac3f8d2a90d18b
Ankit Sahu <ankits6387@gmail.com>
2026-05-16 16:31:58 +0530

    refactored

--- api-gateway/src/main/java/com/hireconnect/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/hireconnect/apigateway/ApiGatewayApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableEurekaClient
 
 public class ApiGatewayApplication {
 
