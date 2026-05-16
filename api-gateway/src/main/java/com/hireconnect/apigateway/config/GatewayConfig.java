6b53994294361d568e78fc704e90fc0fe4c377fd
Ankit Sahu <ankits6387@gmail.com>
2026-05-16 20:41:19 +0530

    refactored

--- api-gateway/src/main/java/com/hireconnect/apigateway/config/GatewayConfig.java
+++ api-gateway/src/main/java/com/hireconnect/apigateway/config/GatewayConfig.java
@@ -11,7 +11,7 @@ public class GatewayConfig {
     @Bean
     public RouteLocator customRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("AUTH-SERVICE", r -> r.path("/auth/**").uri("lb://auth-service"))
+                .route("AUTH-SERVICE", r -> r.path("/auth/**", "/oauth2/**", "/login/oauth2/**").uri("lb://auth-service"))
                 .route("USER-SERVICE", r -> r.path("/users/**").uri("lb://user-service"))
                 .route("JOB-SERVICE", r -> r.path("/jobs/**").uri("lb://job-service"))
                 .route("APPLICATION-SERVICE", r -> r.path("/applications/**").uri("lb://application-service"))
